5bd77adfc31f6ec394c22de4ff64ff3459dbd44f
Han Wei <jensonwei2012@gmail.com>
2019-07-29 20:38:03 -0400

    Create 627. Longest Palindrome.cpp

---
+++ 627. Longest Palindrome.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    /**
+     * @param s: a string which consists of lowercase or uppercase letters
+     * @return: the length of the longest palindromes that can be built
+     */
+    int longestPalindrome(string &s) {
+        if(s.empty()) return 0;
+        int result = 0;
+        int nums[128] = {0};
+        for(int number : s){
+            nums[number]++;
+        }
+        
+        bool odd = false;
+        
+        for(int num: nums){
+            if(num%2==0){
+                result += num;
+            }else{
+                odd = true;
+                result += num-1;
+            }
+        }
+        
+        if(odd){
+            result++;
+        }
+        
+        return result;
+        // write your code here
+    }
+};
